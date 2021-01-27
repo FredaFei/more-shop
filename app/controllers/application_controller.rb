855b96f05624754f49bfe95e72ddbbb35afda631
fh <feih@angelcomm.cn>
2021-01-27 13:58:18 +0800

    修改为API模式

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  def render_resource(resource)
+    return head 404 if resource.nil?
+    render json: {resource: resource}, status: 200
+  end
 end
