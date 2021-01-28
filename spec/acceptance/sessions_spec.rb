c85a71a0a9ddfdc75664e31b3285e3607b27ecf6
fh <feih@angelcomm.cn>
2021-01-28 11:30:31 +0800

    添加sessions测试

--- spec/acceptance/sessions_spec.rb
+++ spec/acceptance/sessions_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 require 'rspec_api_documentation/dsl'
-resource "Users" do
+
+resource "Sessions" do
   let(:user) { create :user }
-  post "/users" do
+  post "/sessions" do
     parameter :phone, '手机号码', type: :string, required: true
     parameter :password, '密码', type: :string, required: true
-    parameter :password_confirmation, '确认密码', type: :string, required: true
-    example "创建用户（注册）" do
-      do_request(phone: user.phone, password: user.password, password_confirmation: user.password_confirmation)
+    example "用户登录" do
+      do_request phone: user.phone, password: user.password
+      expect(status).to eq 200
+    end
+  end
+  delete "/sessions" do
+    example "退出登录" do
+      do_request
       expect(status).to eq 200
     end
   end
