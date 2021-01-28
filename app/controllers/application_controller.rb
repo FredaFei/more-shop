b8b50e6fa13d3656f971072a0b237aa24f27d92b
fh <feih@angelcomm.cn>
2021-01-28 11:29:04 +0800

    添加sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @current_user ||= User.find_by_id session[:current_user_id]
+  end
+  
   def render_resource(resource)
     return head 404 if resource.nil?
-    render json: {resource: resource}, status: 200
+    render json: { resource: resource }, status: 200
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def create
     render_resource User.create create_params
   end
-  
+  def me
+    # render_resource cu
+  end
   def create_params
     params.permit(:phone, :password, :password_confirmation)
   end
