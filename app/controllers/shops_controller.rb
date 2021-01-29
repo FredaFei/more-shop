744f3a65a58ad26280199982c8e38a971c5cdec4
fh <feih@angelcomm.cn>
2021-01-29 18:14:37 +0800

    更新 shop contorller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,12 @@
 class ShopsController < ApplicationController
+  def create
+    shop = Shop.create create_params
+    p 'shop#create---'
+    p shop
+    render_resource shop
+  end
+  
+  def create_params
+    params.permit(:logo)
+  end
 end
